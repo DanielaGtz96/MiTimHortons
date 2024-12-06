991c57a124461868ed0e615d3f481118754d649f
Daniela <danielagtz9653@gmail.com>
2024-12-05 21:43:48 -0500

    borre error de js

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
 function redirigir(url){
     window.location.href = url;
 }
-
-// Filtro por categoría
-function filtrarCategoria(categoria) {
-    const items = document.querySelectorAll('.menu-item');
-    items.forEach(item => {
-        item.style.display = item.dataset.categoria === categoria || categoria === 'todo' ? 'flex' : 'none';
-    });
-}
